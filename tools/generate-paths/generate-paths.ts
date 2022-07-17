459691be226efa88f84fe93c405158c7e819b025
Atulin <koumarin@gmail.com>
2022-07-17 12:23:25 +0200

    dded type generation

--- tools/generate-paths/generate-paths.ts
+++ tools/generate-paths/generate-paths.ts
@@ -1,87 +1,143 @@
-import { ensureDir } from 'https://deno.land/std@0.140.0/fs/mod.ts';
-import { Parameter, Route, SwaggerResponse } from './types.ts';
+import { ensureDir } from "https://deno.land/std@0.140.0/fs/mod.ts";
+import { Component, Parameter, Route, SwaggerResponse, Type } from "./types.ts";
 
 const replaceType = (type: string): string => {
-	return (
-		{
-			integer: 'number',
-			decimal: 'number',
-			double: 'number',
-			float: 'number',
-			bool: 'boolean',
-			string: 'string',
-		}[type] ?? 'any'
-	);
+  return (
+    {
+      integer: "number",
+      decimal: "number",
+      double: "number",
+      float: "number",
+      bool: "boolean",
+      string: "string",
+    }[type] ?? "any"
+  );
 };
 
 const buildParams = (parameters: Map<number, Parameter>): string => {
-	const params = [];
-	for (const [_, param] of Object.entries(parameters)) {
-		if (param.in !== 'path' && param.in !== 'query') continue;
-		let p = param.name;
-		p += ': ';
-		p += replaceType(param.schema.type);
-		params.push(p);
-	}
-	return params.join(', ');
+  const params = [];
+  for (const [_, param] of Object.entries(parameters)) {
+    if (param.in !== "path" && param.in !== "query") continue;
+    let p = param.name;
+    p += ": ";
+    p += replaceType(param.schema.type);
+    params.push(p);
+  }
+  return params.join(", ");
 };
 
 const buildQuery = (parameters: Map<number, Parameter>): string => {
-	const params = [];
-	for (const [_, param] of Object.entries(parameters)) {
-		if (param.in !== 'query') continue;
-		let p = param.name;
-		p += '=${';
-		p += param.name;
-		p += '}';
-		params.push(p);
-	}
-	return params.length > 0 ? `?${params.join('&')}` : '';
+  const params = [];
+  for (const [_, param] of Object.entries(parameters)) {
+    if (param.in !== "query") continue;
+    let p = param.name;
+    p += "=${";
+    p += param.name;
+    p += "}";
+    params.push(p);
+  }
+  return params.length > 0 ? `?${params.join("&")}` : "";
 };
 
-const buildFunction = (path: string, method: string, meta: Route) => {
-	method = method.toUpperCase();
-	const id = meta.operationId;
-	const params = meta.parameters ? buildParams(meta.parameters).toLowerCase() + ', ' : '';
-	const url = path.replaceAll('{', '${').toLowerCase();
-	const query = meta.parameters ? buildQuery(meta.parameters).toLowerCase() : '';
+const buildFunction = (
+  path: string,
+  method: string,
+  meta: Route
+): { func: string; type: string | null } => {
+  method = method.toUpperCase();
+  const id = meta.operationId;
+  const params = meta.parameters
+    ? buildParams(meta.parameters).toLowerCase() + ", "
+    : "";
+  const url = path.replaceAll("{", "${").toLowerCase();
+  const query = meta.parameters
+    ? buildQuery(meta.parameters).toLowerCase()
+    : "";
 
-	const signature = `async (${params}headers?: HeadersInit, options?: RequestInit)`;
+  const bodyRef = meta.requestBody?.content["application/json"]?.schema?.$ref;
+  const bodyType = bodyRef?.split("/").at(-1);
+  const body = bodyType ? `body: ${bodyType}, ` : "";
 
-	return `export const ${id} = ${signature} => await fetch (\`${url}${query}\`, { 
+  const signature = `async (${body}${params}headers?: HeadersInit, options?: RequestInit)`;
+
+  const func = `export const ${id} = ${signature} => await fetch (\`${url}${query}\`, { 
     method: '${method}', 
     headers: { 
       'Content-Type': 'application/json', 
       ...headers 
-    }, 
+    },${bodyType ? "\n    body: JSON.stringify(body)," : ""}
     ...options 
   });`;
+
+  return { func, type: bodyType ?? null };
 };
 
-const generate = async (path: string): Promise<string[]> => {
-	const res = await fetch(path);
-	const data: SwaggerResponse = await res.json();
+const buildType = (name: string, component: Component): string | null => {
+  if (!component.properties) return null;
 
-	const paths = [];
+  const typeMappings: { [key: string]: string } = {
+    integer: "number",
+    undefined: "object",
+    array: "object[]",
+  };
 
-	for (const [k, v] of Object.entries(data.paths)) {
-		for (const [pathKey, pathVal] of Object.entries(v)) {
-			const fun = buildFunction(k, pathKey, pathVal as Route);
-			paths.push(fun);
-		}
-	}
+  let type = `export interface ${name} {\n`;
 
-	return paths;
+  for (const [k, v] of Object.entries(component.properties)) {
+    const t = typeMappings[v.type] ?? v.type;
+    type += `    ${k}: ${t}${v.nullable ? " | null" : ""};\n`;
+  }
+
+  type += "}";
+  return type;
+};
+
+const generate = async (
+  path: string
+): Promise<{
+  paths: string[];
+  types: string[];
+  typeImports: (string | null)[];
+}> => {
+  const res = await fetch(path);
+  const data: SwaggerResponse = await res.json();
+
+  const paths = [];
+  const typeImports: (string | null)[] = [];
+
+  for (const [k, v] of Object.entries(data.paths)) {
+    for (const [pathKey, pathVal] of Object.entries(v)) {
+      const { func, type } = buildFunction(k, pathKey, pathVal as Route);
+      paths.push(func);
+      if (type) typeImports.push(type);
+    }
+  }
+
+  const types = [];
+  for (const [k, v] of Object.entries(data.components.schemas)) {
+    const type = buildType(k, v);
+    if (type === null) continue;
+    types.push(type);
+  }
+
+  return { paths, types, typeImports };
 };
 
 export const generatePaths = async (
-	outDir: string,
-	paths: { [key: string]: string },
+  outDir: string,
+  paths: { [key: string]: string }
 ) => {
-	ensureDir(outDir);
-	for (const [key, val] of Object.entries(paths)) {
-		console.log(`Generating paths for: ${key}`);
-		const paths = await generate(val);
-		await Deno.writeTextFile(`${outDir}/paths-${key}.ts`, paths.join('\n\n'));
-	}
+  ensureDir(outDir);
+  for (const [key, val] of Object.entries(paths)) {
+    console.log(`Generating paths for: ${key}`);
+    const { paths, types, typeImports } = await generate(val);
+
+    const pathsFile = `import {
+\t${typeImports.join(",\n\t")}
+} from './types-${key}';\n
+${paths.join("\n\n")}`;
+
+    await Deno.writeTextFile(`${outDir}/paths-${key}.ts`, pathsFile);
+    await Deno.writeTextFile(`${outDir}/types-${key}.ts`, types.join("\n\n"));
+  }
 };
--- tools/generate-paths/types.ts
+++ tools/generate-paths/types.ts
@@ -1,20 +1,37 @@
 export interface SwaggerResponse {
-	'x-generator': string;
-	openapi: string;
-	info: { title: string; version: string };
-	paths: Map<string, Path>;
+  "x-generator": string;
+  openapi: string;
+  info: { title: string; version: string };
+  paths: Map<string, Path>;
+  components: { schemas: Map<string, Component> };
 }
 
 export type Path = Map<string, Route>;
 
 export interface Route {
-	tags: string[];
-	operationId: string;
-	parameters?: Map<number, Parameter>;
+  tags: string[];
+  operationId: string;
+  parameters?: Map<number, Parameter>;
+  requestBody: RequestBody | null;
 }
 
 export interface Parameter {
-	name: string;
-	in: string;
-	schema: { type: string; nullable: boolean };
+  name: string;
+  in: string;
+  schema: { type: string; nullable: boolean };
+}
+
+export interface RequestBody {
+  "x-name": string;
+  content: { "application/json": { schema: { $ref: string } } };
+}
+
+export interface Component {
+  properties: Map<string, Type>;
+}
+
+export interface Type {
+  type: string;
+  format: string | undefined;
+  nullable: boolean | undefined;
 }
